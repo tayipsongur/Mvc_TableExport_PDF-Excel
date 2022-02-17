e7d2590047ad98c444c94f57f5973f4ff69d8e3d
Tayip <Tayip@DESKTOP-AIKEIHG>
2022-02-17 17:00:17 +0300

    Add project files.

---
+++ Export/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Export.Controllers;
+using Export.Models.Entity;
+using Excel = Microsoft.Office.Interop.Excel;
+
+
+namespace Export.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+
+        KargoEntities db = new KargoEntities();
+        public ActionResult Index()
+        {
+
+
+            var listele = db.Musteriler.ToList();
+
+            return View(listele);
+        }
+    }
+}
